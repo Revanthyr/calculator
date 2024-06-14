9a11970806fe4cb06ad56bf94b5fd25f2eaa010e
Revanthyr <155083567+Revanthyr@users.noreply.github.com>
2024-06-14 17:42:21 +0200

    Add basic operations

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ function divide(arg1,arg2){
 }
 
 
-let number1 = 0
-let number2 = 1
-let operator = "+"
+let number1 = 0;
+let number2 = 1;
+let operator = "+";
+
 
 function operate(num1,num2,op){
     if (op == "+"){
@@ -30,6 +31,55 @@ function operate(num1,num2,op){
         return multiply(num1,num2)
     }
 }
+
+let object = {
+    value1: 0,
+    operator:"",
+    value2:0
+}
+let counter = 0;
 let button =""
 const body =document.querySelector("body");
 const container = document.querySelector(".container");
+const display = document.querySelector(".display");
+display.textContent = "";
+const numberButtons = document.querySelectorAll(".number")
+numberButtons.forEach(button =>{
+    button.addEventListener("click",()=>{
+        if (display.textContent.length <25 && counter != 1){
+            
+        display.textContent = display.textContent.concat(button.textContent)}
+        else if (counter == 1){
+            display.textContent = button.textContent;
+            counter +=1;
+        }
+    })
+})
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(current =>{
+    current.addEventListener("click",() => { 
+        counter = 1
+        object.operator = current.textContent;
+        object.value1 = display.textContent;
+    })
+})
+const equal = document.querySelector(".equals")
+equal.addEventListener("click",a =>{
+    counter = 1;
+    object.value2 = display.textContent;
+    display.textContent = operate(object.value1,object.value2,object.operator)
+})
+
+
+/* 
+e
+ ALTERNATVE
+
+ WHEN NUMBER PRESSED => chnage the display text conent => if counter is 1 then dont concat and change counter to 2
+ WHEN OPERATOR PRESSED => store the current text  content and the operator and set a counter to 1
+ WHEN EQUAL PRESSED => store the current display text content and operate and reset counter to 0
+
+
+*/
+
